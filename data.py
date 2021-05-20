3621e55f5f1545016cde3642b5d93ca0a8e48226
Guillaume Pavé <guillaumepave@gmail.com>
2021-05-20 08:53:17 +0200

    add some descr stat

--- data.py
+++ data.py
@@ -18,23 +18,24 @@ class Data:
 
     def load_data(self):
         try:
-            self.df = pd.read_csv(f'./data/processed/{self.crypto_name}_finaldb_fct.csv').dropna()
+            self.df = pd.read_csv(f'./data/processed/{self.crypto_name}_finaldb.csv').dropna()
         except FileNotFoundError:
-            self.df = pd.read_csv(f'../data/processed/{self.crypto_name}_finaldb_fct.csv').dropna()
+            self.df = pd.read_csv(f'../data/processed/{self.crypto_name}_finaldb.csv').dropna()
         self.df = self.df.iloc[:,1:]
         self.df['Close_std'] = (self.df['Close']-self.df['Close'].mean())/self.df['Close'].std()
-        self.df['Close_ret'] = np.log(self.df[['Close']].shift(-1).values/self.df[['Close']].values)
-    
-    def create_RNN_data(self,LAG=10,reg="Price"):
+        self.df['Close_ret_t+1'] = np.log(self.df[['Close']].shift(-1).values/self.df[['Close']].values)
+        self.df['pump_5'] = np.where(self.df['Close_ret_t+1'] > 0.05,1,0)
+
+    def create_RNN_data(self,LAG=10,reg="Price",columns=["Volume","Close"]):
         
         if reg == "Price":
             y=self.df[['Close_std']].values
         else:
-            y= self.df[['Close_ret']].iloc[1:,:].values
+            y= self.df[['Close_ret_t+1']].iloc[1:,:].values
         
         #print(y)
         X = []
-        xx = self.df[["Volume","MA_100","coinmarketcap","justinsuntron","tweet_count"]]
+        xx = self.df[columns]
         for i in range(LAG+1,0-1,-1):
             if i > 0:
                 X.append(xx[LAG+1 - i:-i])
@@ -47,10 +48,11 @@ class Data:
         X = X.reshape((X.shape[0], X.shape[1], 1)) # add the input dimension !
        
         y = y[:-1,:]
-        #==================
-        ## Classification ##
-        #==================
-        y = np.concatenate((np.where(y>0.05,1),np.where(y<=0.05,1)),axis=1)
+
+        #======================
+        # LSTM classification #
+        #======================
+        y = np.concatenate((np.where(y>0.05,1,0),np.where(y<=0.05,1,0)),axis=1)
 
         #standardize X
         X = X[:-1,:]
@@ -65,3 +67,9 @@ class Data:
         self.y_tr = y[np.where(ind[:tr])[0], :]
         self.y_te = y[np.where(ind[tr:te])[0], :]
         self.y_va = y[np.where(ind[te:])[0], :]
+
+if __name__ == "__main__":
+    data = Data("BTC")
+    data.load_data()
+    #choose for our estimation
+    data.create_RNN_data(reg='return',LAG=10)
--- models.py
+++ models.py
@@ -7,24 +7,25 @@ import sys
 from data import Data
 from NN_models import RNN
 
-data = Data("ETH")
+data = Data("BTC")
 data.load_data()
 #choose for our estimation
 data.create_RNN_data(reg='return',LAG=10)
-#data.df
+data.df
 
+#%%
 model=RNN()
 
-model.create_model(data,architecture=[10],batch_normalization=False,activation="relu",drop_out=0.2)
+model.create_model(data,architecture=[10],batch_normalization=True,activation="relu",drop_out=0.2)
 model.train_model(data,verbose=0,epoch=10)
 model.show_performance(label_='Error graph',data=data)
 pred=model.model(data.X_te)
 
 
-plt.plot(pred.numpy().flatten(),color='k',label='Pred')
-plt.plot(data.y_te.flatten(),color='blue',linestyle='--',label='True')
-plt.legend()
-plt.show()
+# plt.plot(pred.numpy().flatten(),color='k',label='Pred')
+# plt.plot(data.y_te.flatten(),color='blue',linestyle='--',label='True')
+# plt.legend()
+# plt.show()
 
 
 
