2457eccb2ce349ea6c6d39eb4057237b25be6890
dim3003 <dimitri.andre@unil.ch>
2021-05-24 15:15:19 +0200

    POSIX for fetch

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,15 +1,21 @@
 #utilities
 import os
+from pathlib import Path
 
 #project modules
 import fetchScripts.getCryptodata as cryptoRaw
 import fetchScripts.twitterdata as twitterRaw
 
+path_original = Path(__file__).resolve().parents[1]
+path_data_origin = (path_original / "./data/").resolve()
+path_data = (path_original / "./data/raw/").resolve()
+path_data_processed = (path_original / "./data/processed/").resolve()
+
 def fetchData():
     #creates data directory if it does not exists
-    if not os.path.isdir('../data'):
-        os.mkdir('../data')
-        os.mkdir('../data/raw')
+    if not os.path.isdir(str(path_data_origin)):
+        os.mkdir(str(path_data))
+        os.mkdir(str(path_data))
 
 
     cryptoRaw.getRawCrypto()
